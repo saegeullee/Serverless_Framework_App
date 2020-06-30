3a492b260ea0a1779656eecfd17ac918feee5be9
saegeullee <lemix777@naver.com>
2020-06-30 19:20:49 +0900

    Implemented sending messages to all websocket connected clients when new image is uploaded

--- src/lambda/s3/sendNotifications.ts
+++ src/lambda/s3/sendNotifications.ts
@@ -1,9 +1,66 @@
 import { S3Handler, S3Event } from 'aws-lambda';
+import * as AWS from 'aws-sdk';
 import 'source-map-support/register';
+
+const docClient = new AWS.DynamoDB.DocumentClient();
+
+const connectionsTable = process.env.CONNECTIONS_TABLE;
+const stage = process.env.STAGE;
+const apiId = process.env.API_ID;
+
+const connectionParams = {
+  apiVersion: '2018-11-29',
+  endpoint: `${apiId}.execute-api.us-east-1.amazonaws.com/${stage}`
+};
+
+const apiGateway = new AWS.ApiGatewayManagementApi(connectionParams);
 
 export const handler: S3Handler = async (event: S3Event) => {
   for (const record of event.Records) {
     const key = record.s3.object.key;
     console.log('Processing s3 item with key: ', key);
+
+    const connections = await docClient
+      .scan({
+        TableName: connectionsTable
+      })
+      .promise();
+
+    const payload = {
+      imageId: key
+    };
+
+    for (const connection of connections.Items) {
+      const connectionId = connection.id;
+      await sendMessageToClient(connectionId, payload);
+    }
   }
 };
+
+async function sendMessageToClient(connectionId, payload) {
+  try {
+    console.log('Sending message to a connection ', connectionId);
+
+    await apiGateway
+      .postToConnection({
+        ConnectionId: connectionId,
+        Data: JSON.stringify(payload)
+      })
+      .promise();
+  } catch (e) {
+    console.log('Failed to send message ', JSON.stringify(e));
+    // connection 이 끊어졌지만 아직 db 테이블에 해당 커넥션 id가 record에 남아 있는경우 410 코드를 던진다
+    if (e.statusCode === 410) {
+      console.log('Stale connection');
+
+      await docClient
+        .delete({
+          TableName: connectionsTable,
+          Key: {
+            id: connectionId
+          }
+        })
+        .promise();
+    }
+  }
+}
