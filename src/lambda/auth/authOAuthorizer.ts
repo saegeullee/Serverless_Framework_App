34b53e73d7dc1633fbc194ac8fe02306e3f3e928
saegeullee <lemix777@naver.com>
2020-07-04 17:02:02 +0900

    Implemented storing secret key in AWS secret manager rather than storing it as environment variable

--- src/lambda/auth/authOAuthorizer.ts
+++ src/lambda/auth/authOAuthorizer.ts
@@ -1,15 +1,23 @@
 import { CustomAuthorizerEvent, CustomAuthorizerResult, CustomAuthorizerHandler } from 'aws-lambda';
 import 'source-map-support/register';
+import * as AWS from 'aws-sdk';
+
 import { verify } from 'jsonwebtoken';
 import { JwtToken } from '../../auth/JwtToken';
 
-const auth0Secret = process.env.AUTH_0_SECRET;
+const secretId = process.env.AUTH_0_SECRET_ID;
+const secretField = process.env.AUTH_0_SECRET_FIELD;
+
+const client = new AWS.SecretsManager();
+
+// Cache secret if a Lambda instance is reused
+let cachedSecret: string;
 
 export const handler: CustomAuthorizerHandler = async (
   event: CustomAuthorizerEvent
 ): Promise<CustomAuthorizerResult> => {
   try {
-    const decodedToken = verifyToken(event.authorizationToken);
+    const decodedToken = await verifyToken(event.authorizationToken);
     console.log('User was authorized');
 
     return {
@@ -44,7 +52,7 @@ export const handler: CustomAuthorizerHandler = async (
   }
 };
 
-function verifyToken(authHeader: string): JwtToken {
+async function verifyToken(authHeader: string): Promise<JwtToken> {
   if (!authHeader) throw new Error('No authorization header');
 
   if (!authHeader.toLocaleLowerCase().startsWith('bearer '))
@@ -53,5 +61,21 @@ function verifyToken(authHeader: string): JwtToken {
   const split = authHeader.split(' ');
   const token = split[1];
 
-  return verify(token, auth0Secret) as JwtToken;
+  const secretObject: any = await getSecret();
+  const secret = secretObject[secretField];
+
+  return verify(token, secret) as JwtToken;
+}
+
+async function getSecret() {
+  if (cachedSecret) return cachedSecret;
+
+  const data = await client
+    .getSecretValue({
+      SecretId: secretId
+    })
+    .promise();
+
+  cachedSecret = data.SecretString;
+  return JSON.parse(cachedSecret);
 }
