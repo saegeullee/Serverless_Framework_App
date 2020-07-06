443984fe3ba0b650f33ce6984521774bb9fa9312
saegeullee <lemix777@naver.com>
2020-07-07 02:09:28 +0900

    Implemented using middy middleware to fetch secret key

--- src/lambda/auth/authOAuthorizer.ts
+++ src/lambda/auth/authOAuthorizer.ts
@@ -1,58 +1,55 @@
-import { CustomAuthorizerEvent, CustomAuthorizerResult, CustomAuthorizerHandler } from 'aws-lambda';
+import { CustomAuthorizerEvent, CustomAuthorizerResult } from 'aws-lambda';
 import 'source-map-support/register';
-import * as AWS from 'aws-sdk';
 
 import { verify } from 'jsonwebtoken';
 import { JwtToken } from '../../auth/JwtToken';
 
+import * as middy from 'middy';
+import { secretsManager } from 'middy/middlewares';
+
 const secretId = process.env.AUTH_0_SECRET_ID;
 const secretField = process.env.AUTH_0_SECRET_FIELD;
 
-const client = new AWS.SecretsManager();
+export const handler = middy(
+  async (event: CustomAuthorizerEvent, context): Promise<CustomAuthorizerResult> => {
+    try {
+      const decodedToken = verifyToken(event.authorizationToken, context.AUTH0_SECRET[secretField]);
+      console.log('User was authorized');
 
-// Cache secret if a Lambda instance is reused
-let cachedSecret: string;
+      return {
+        principalId: decodedToken.sub,
+        policyDocument: {
+          Version: '2012-10-17',
+          Statement: [
+            {
+              Action: 'execute-api:Invoke',
+              Effect: 'Allow',
+              Resource: '*'
+            }
+          ]
+        }
+      };
+    } catch (e) {
+      console.log('User was not authorized', e.message);
 
-export const handler: CustomAuthorizerHandler = async (
-  event: CustomAuthorizerEvent
-): Promise<CustomAuthorizerResult> => {
-  try {
-    const decodedToken = await verifyToken(event.authorizationToken);
-    console.log('User was authorized');
-
-    return {
-      principalId: decodedToken.sub,
-      policyDocument: {
-        Version: '2012-10-17',
-        Statement: [
-          {
-            Action: 'execute-api:Invoke',
-            Effect: 'Allow',
-            Resource: '*'
-          }
-        ]
-      }
-    };
-  } catch (e) {
-    console.log('User was not authorized', e.message);
-
-    return {
-      principalId: 'user',
-      policyDocument: {
-        Version: '2012-10-17',
-        Statement: [
-          {
-            Action: 'execute-api:Invoke',
-            Effect: 'Deny',
-            Resource: '*'
-          }
-        ]
-      }
-    };
+      return {
+        principalId: 'user',
+        policyDocument: {
+          Version: '2012-10-17',
+          Statement: [
+            {
+              Action: 'execute-api:Invoke',
+              Effect: 'Deny',
+              Resource: '*'
+            }
+          ]
+        }
+      };
+    }
   }
-};
+);
 
-async function verifyToken(authHeader: string): Promise<JwtToken> {
+function verifyToken(authHeader: string, secret: string): JwtToken {
   if (!authHeader) throw new Error('No authorization header');
 
   if (!authHeader.toLocaleLowerCase().startsWith('bearer '))
@@ -61,21 +58,16 @@ async function verifyToken(authHeader: string): Promise<JwtToken> {
   const split = authHeader.split(' ');
   const token = split[1];
 
-  const secretObject: any = await getSecret();
-  const secret = secretObject[secretField];
-
   return verify(token, secret) as JwtToken;
 }
 
-async function getSecret() {
-  if (cachedSecret) return cachedSecret;
-
-  const data = await client
-    .getSecretValue({
-      SecretId: secretId
-    })
-    .promise();
-
-  cachedSecret = data.SecretString;
-  return JSON.parse(cachedSecret);
-}
+handler.use(
+  secretsManager({
+    cache: true,
+    cacheExpiryInMillis: 60000,
+    throwOnFailedCall: true,
+    secrets: {
+      AUTH0_SECRET: secretId
+    }
+  })
+);
